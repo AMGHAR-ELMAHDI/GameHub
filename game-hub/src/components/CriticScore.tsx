9c89520340685f619f098cbd86e68db04105fb2f
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-05-21 18:40:17 +0100

    displayed critic score

---
+++ game-hub/src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+    let color  = score >= 75 ? "green" : score >= 50 ? "yellow" : "red";
+  return (
+    <>
+      <Badge colorScheme={color} fontSize={"14px"} paddingX={2} borderRadius={"4px"}>
+        {score}
+      </Badge>
+    </>
+  );
+}
+
+export default CriticScore;
--- game-hub/src/components/PlatformIconList.tsx
+++ game-hub/src/components/PlatformIconList.tsx
@@ -1,5 +1,9 @@
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
 import {
   FaWindows,
   FaPlaystation,
@@ -9,16 +13,12 @@ import {
   FaAndroid,
 } from "react-icons/fa";
 
-import { MdPhoneIphone } from "react-icons/md";
-import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-
 interface Prosp {
   platforms: Platform[];
 }
 
 function PlatformIconList({ platforms }: Prosp) {
-  const iconMap = {
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
@@ -31,11 +31,15 @@ function PlatformIconList({ platforms }: Prosp) {
   };
 
   return (
-    <>
+    <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Text key={platform.id}>{platform.name}</Text>
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
       ))}
-    </>
+    </HStack>
   );
 }
 
