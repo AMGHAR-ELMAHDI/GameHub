4bf0539a63d7229d88c17e679bb0179d716b6dc6
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-05-21 11:59:24 +0100

    Started Working

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg={"dodgerblue"}>
+          main
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
